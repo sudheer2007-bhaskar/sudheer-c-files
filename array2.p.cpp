a003ee09c57778d1691ca44813b64a8a3f83a367
sudheer2007-bhaskar <s91752971@gmail.com>
2026-02-21 20:35:57 +0530

    Add files via upload

---
+++ array2.p.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main ()
+{
+int a[10],i,pos,ele;
+printf("enter the numbers in array");
+for(i=0;i<5;i++);
+  {
+  	scanf("%d",&a[i]);
+  }
+printf("enter the position");
+scanf("%d",&pos);
+printf("enter the element");
+scanf("%d",&ele);
+for(i=4;i>=pos;i--)
+{
+a[i+1]=a[i];
+}
+a[pos]=ele;
+for(i=0;i<=5;i++)
+{
+printf("%d",a[i]);
+}
+return 0;
+
+
+}
